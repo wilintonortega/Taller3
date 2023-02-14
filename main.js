3371706665215661c1f3c1a403d5bac1949e12a1
wili1991 <wikki9106@gmail.com>
2023-02-14 12:55:03 -0500

    punto 7

--- main.js
+++ main.js
@@ -39,29 +39,44 @@
 // }
 
 // 6.	Realizar un programa que lea N números, calcule y escriba la suma de los pares y el producto de los impares.
-let sumap=0;
-let prodimp=1;
-let num=parseInt(prompt("Ingrese un numero, \" para detener el programa escriba 0\" "));
+// let sumap=0;
+// let prodimp=1;
+// let num=parseInt(prompt("Ingrese un numero, \" para detener el programa escriba 0\" "));
 
-while (num !=0) {
-        //Ingrese un numero, \" para detener el programa escriba 0\" 
+// while (num !=0) {
+//         //Ingrese un numero, \" para detener el programa escriba 0\" 
     
-        if(num%2==0){
-            sumap += num;
-            alert("la suma de pares es "+sumap);
-        }else{
-            alert("impar")
-            prodimp *=num;
-            alert("El producto de impares es: "+prodimp);
-        }
+//         if(num%2==0){
+//             sumap += num;
+//             alert("la suma de pares es "+sumap);
+//         }else{
+//             alert("impar")
+//             prodimp *=num;
+//             alert("El producto de impares es: "+prodimp);
+//         }
      
-        num=parseInt(prompt("Ingrese un numero, \" para detener el programa escriba 0\" "));
-}
-alert(`El producto final de impares es ${prodimp} Y La suma final de pares es ${sumap}`);
+//         num=parseInt(prompt("Ingrese un numero, \" para detener el programa escriba 0\" "));
+// }
+// alert(`El producto final de impares es ${prodimp} Y La suma final de pares es ${sumap}`);
 
 
 // 7.	Dados dos números enteros positivos N y D, se dice que D es un divisor de N si el resto de dividir N entre D es 0. Se dice que un número N es perfecto si la suma de sus divisores (excluido el propio N) es N. Por ejemplo 28 es perfecto, pues sus divisores (excluido elv28) son: 1, 2, 4, 7 y 14 y su suma es 1+2+4+7+14=28. Hacer un organigrama que dado un número N nos diga si es o no perfecto.
 
+let num=parseInt(prompt("Digite un numero para saber si es \"Perfecto\" o No "));
+let suma=0;
+    for (let i = 1; i < num; i++) 
+    {
+        if (num%i==0) 
+        {
+         suma +=i;   
+        }
+            //PROBANDO SUMA Y SI VALOR ES PERFECTO
+            //alert(suma);
+            // if(suma==num){
+            //     alert("Es un numero perfecto");
+            //     }
+    }
+    (suma==num)?alert("Es un numero perfecto"):alert("No es un numero perfecto");
 
 
 // Nota: En los archivos index.html mostrar en la pantalla un mensaje que diga “Ver la consola del navegador o oprima la tecla F12 para ver los resultados”. Si utiliza la función console.log(); de lo contrario si utiliza alert(); No mostrar nada en el index.html
