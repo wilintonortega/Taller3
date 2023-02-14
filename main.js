45124ae17fc8edafda93d11cb1763b296df3ef55
wili1991 <wikki9106@gmail.com>
2023-02-14 03:42:44 -0500

    Taller3

---
+++ main.js
@@ -0,0 +1,23 @@
+// //Taller 3
+
+// 1.	Realizar un programa que permita leer 2 números diferentes y nos diga cual es el mayor de los 2 números.
+
+let num1=Number(prompt("Ingrese el numero 1"));
+let num2=Number(prompt("Ingrese el numero 2"));
+
+(num1>num2)?alert(`el ${num1} es mayor a ${num2}`):alert(`el ${num2} es mayor a ${num1}`);
+
+
+// 2.	Realice un programa que calcule el máximo de N números leídos desde teclado.
+
+// 3.	Realizar un programa que pueda sumar dos números leídos por teclado y escribir el resultado.	
+
+// 4.	Realizar un programa de procesos en el que se almacenen 3 números en 3 variables A, B y C. El diagrama debe decidir cual es el mayor y cual es el menor
+
+// 5.	Realizar un programa que permita escribir los 100 primeros pares.
+
+// 6.	Realizar un programa que lea N números, calcule y escriba la suma de los pares y el producto de los impares.
+
+// 7.	Dados dos números enteros positivos N y D, se dice que D es un divisor de N si el resto de dividir N entre D es 0. Se dice que un número N es perfecto si la suma de sus divisores (excluido el propio N) es N. Por ejemplo 28 es perfecto, pues sus divisores (excluido elv28) son: 1, 2, 4, 7 y 14 y su suma es 1+2+4+7+14=28. Hacer un organigrama que dado un número N nos diga si es o no perfecto.
+
+// Nota: En los archivos index.html mostrar en la pantalla un mensaje que diga “Ver la consola del navegador o oprima la tecla F12 para ver los resultados”. Si utiliza la función console.log(); de lo contrario si utiliza alert(); No mostrar nada en el index.html
