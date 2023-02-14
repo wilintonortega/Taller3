7be7cbfe2329c6e7a5d909df3d42cbae65261bc6
wili1991 <wikki9106@gmail.com>
2023-02-14 12:39:56 -0500

    punto 6

--- main.js
+++ main.js
@@ -31,15 +31,33 @@
 
 // 5.	Realizar un programa que permita escribir los 100 primeros pares.
 
-let par;
-for (let i = 2; i <= 100; i++) {
-    if(i%2==0){
-        alert(i)
-    }
-}
+// let par;
+// for (let i = 2; i <= 100; i++) {
+//     if(i%2==0){
+//         alert(i)
+//     }
+// }
 
 // 6.	Realizar un programa que lea N números, calcule y escriba la suma de los pares y el producto de los impares.
+let sumap=0;
+let prodimp=1;
+let num=parseInt(prompt("Ingrese un numero, \" para detener el programa escriba 0\" "));
 
+while (num !=0) {
+        //Ingrese un numero, \" para detener el programa escriba 0\" 
+    
+        if(num%2==0){
+            sumap += num;
+            alert("la suma de pares es "+sumap);
+        }else{
+            alert("impar")
+            prodimp *=num;
+            alert("El producto de impares es: "+prodimp);
+        }
+     
+        num=parseInt(prompt("Ingrese un numero, \" para detener el programa escriba 0\" "));
+}
+alert(`El producto final de impares es ${prodimp} Y La suma final de pares es ${sumap}`);
 
 
 // 7.	Dados dos números enteros positivos N y D, se dice que D es un divisor de N si el resto de dividir N entre D es 0. Se dice que un número N es perfecto si la suma de sus divisores (excluido el propio N) es N. Por ejemplo 28 es perfecto, pues sus divisores (excluido elv28) son: 1, 2, 4, 7 y 14 y su suma es 1+2+4+7+14=28. Hacer un organigrama que dado un número N nos diga si es o no perfecto.
